1cd50583c412671134c6a9aeefb14eef00e6a699
isss7 <faisfidi@gmail.com>
2021-11-15 13:28:29 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿
+namespace ClientProxyForm_P5_010
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.labelPenjumlahan1 = new System.Windows.Forms.Label();
+            this.labelPerkalian1 = new System.Windows.Forms.Label();
+            this.labelPengurangan1 = new System.Windows.Forms.Label();
+            this.labelPembagian1 = new System.Windows.Forms.Label();
+            this.buttonHasil = new System.Windows.Forms.Button();
+            this.labelPenjumlahan = new System.Windows.Forms.Label();
+            this.labelPengurangan = new System.Windows.Forms.Label();
+            this.labelPerkalian = new System.Windows.Forms.Label();
+            this.labelPembagian = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(135, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Angka 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(135, 211);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Angka 2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(209, 121);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(216, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(209, 208);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(216, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // labelPenjumlahan1
+            // 
+            this.labelPenjumlahan1.AutoSize = true;
+            this.labelPenjumlahan1.Location = new System.Drawing.Point(472, 124);
+            this.labelPenjumlahan1.Name = "labelPenjumlahan1";
+            this.labelPenjumlahan1.Size = new System.Drawing.Size(133, 17);
+            this.labelPenjumlahan1.TabIndex = 4;
+            this.labelPenjumlahan1.Text = "Hasil Penjumlahan: ";
+            // 
+            // labelPerkalian1
+            // 
+            this.labelPerkalian1.AutoSize = true;
+            this.labelPerkalian1.Location = new System.Drawing.Point(472, 211);
+            this.labelPerkalian1.Name = "labelPerkalian1";
+            this.labelPerkalian1.Size = new System.Drawing.Size(110, 17);
+            this.labelPerkalian1.TabIndex = 5;
+            this.labelPerkalian1.Text = "Hasil Perkalian: ";
+            // 
+            // labelPengurangan1
+            // 
+            this.labelPengurangan1.AutoSize = true;
+            this.labelPengurangan1.Location = new System.Drawing.Point(472, 165);
+            this.labelPengurangan1.Name = "labelPengurangan1";
+            this.labelPengurangan1.Size = new System.Drawing.Size(137, 17);
+            this.labelPengurangan1.TabIndex = 6;
+            this.labelPengurangan1.Text = "Hasil Pengurangan: ";
+            // 
+            // labelPembagian1
+            // 
+            this.labelPembagian1.AutoSize = true;
+            this.labelPembagian1.Location = new System.Drawing.Point(472, 256);
+            this.labelPembagian1.Name = "labelPembagian1";
+            this.labelPembagian1.Size = new System.Drawing.Size(122, 17);
+            this.labelPembagian1.TabIndex = 7;
+            this.labelPembagian1.Text = "Hasil Pembagian: ";
+            // 
+            // buttonHasil
+            // 
+            this.buttonHasil.Location = new System.Drawing.Point(350, 302);
+            this.buttonHasil.Name = "buttonHasil";
+            this.buttonHasil.Size = new System.Drawing.Size(75, 23);
+            this.buttonHasil.TabIndex = 8;
+            this.buttonHasil.Text = "Hasil";
+            this.buttonHasil.UseVisualStyleBackColor = true;
+            this.buttonHasil.Click += new System.EventHandler(this.buttonHasil_Click);
+            // 
+            // labelPenjumlahan
+            // 
+            this.labelPenjumlahan.AutoSize = true;
+            this.labelPenjumlahan.Location = new System.Drawing.Point(611, 126);
+            this.labelPenjumlahan.Name = "labelPenjumlahan";
+            this.labelPenjumlahan.Size = new System.Drawing.Size(0, 17);
+            this.labelPenjumlahan.TabIndex = 9;
+            // 
+            // labelPengurangan
+            // 
+            this.labelPengurangan.AutoSize = true;
+            this.labelPengurangan.Location = new System.Drawing.Point(615, 165);
+            this.labelPengurangan.Name = "labelPengurangan";
+            this.labelPengurangan.Size = new System.Drawing.Size(0, 17);
+            this.labelPengurangan.TabIndex = 10;
+            // 
+            // labelPerkalian
+            // 
+            this.labelPerkalian.AutoSize = true;
+            this.labelPerkalian.Location = new System.Drawing.Point(588, 213);
+            this.labelPerkalian.Name = "labelPerkalian";
+            this.labelPerkalian.Size = new System.Drawing.Size(0, 17);
+            this.labelPerkalian.TabIndex = 11;
+            // 
+            // labelPembagian
+            // 
+            this.labelPembagian.AutoSize = true;
+            this.labelPembagian.Location = new System.Drawing.Point(600, 256);
+            this.labelPembagian.Name = "labelPembagian";
+            this.labelPembagian.Size = new System.Drawing.Size(0, 17);
+            this.labelPembagian.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelPembagian);
+            this.Controls.Add(this.labelPerkalian);
+            this.Controls.Add(this.labelPengurangan);
+            this.Controls.Add(this.labelPenjumlahan);
+            this.Controls.Add(this.buttonHasil);
+            this.Controls.Add(this.labelPembagian1);
+            this.Controls.Add(this.labelPengurangan1);
+            this.Controls.Add(this.labelPerkalian1);
+            this.Controls.Add(this.labelPenjumlahan1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label labelPenjumlahan1;
+        private System.Windows.Forms.Label labelPerkalian1;
+        private System.Windows.Forms.Label labelPengurangan1;
+        private System.Windows.Forms.Label labelPembagian1;
+        private System.Windows.Forms.Button buttonHasil;
+        private System.Windows.Forms.Label labelPenjumlahan;
+        private System.Windows.Forms.Label labelPengurangan;
+        private System.Windows.Forms.Label labelPerkalian;
+        private System.Windows.Forms.Label labelPembagian;
+    }
+}
+
